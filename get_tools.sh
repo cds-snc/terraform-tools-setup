d3116e952e2643f543202fd425dad95d4f21cada
Max Neuvians <max.neuvians@cds-snc.ca>
2022-03-10 09:34:32 -0500

    fix: namespace collisions

--- get_tools.sh
+++ get_tools.sh
@@ -15,9 +15,9 @@ echo "Getting conftest ${CONFTEST_VERSION} ..."
 wget "https://github.com/open-policy-agent/conftest/releases/download/v${CONFTEST_VERSION}/conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz"
 wget "https://github.com/open-policy-agent/conftest/releases/download/v${CONFTEST_VERSION}/checksums.txt"
 grep 'Linux_x86_64.tar.gz' < checksums.txt | sha256sum --check  --status
-tar -zxvf "conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz" conftest
-chmod +x conftest
-mv conftest ${BIN_DIR}
+tar -zxvf "conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz" conftest_binary
+chmod +x conftest_binary
+mv conftest_binary ${BIN_DIR}/conftest
 rm "conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz" checksums.txt
 echo "Done downloading conftest ${CONFTEST_VERSION}"
 
@@ -27,9 +27,9 @@ echo "Getting terraform ${TERRAFORM_VERSION} ..."
 wget "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 wget "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS"
 grep 'linux_amd64.zip' < terraform_${TERRAFORM_VERSION}_SHA256SUMS | sha256sum --check  --status
-unzip "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" terraform
-chmod +x terraform
-mv terraform ${BIN_DIR}
+unzip "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" terraform_binary
+chmod +x terraform_binary
+mv terraform_binary ${BIN_DIR}/terraform
 rm "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" terraform_${TERRAFORM_VERSION}_SHA256SUMS
 echo "Done downloading terraform ${TERRAFORM_VERSION}"
 
@@ -39,8 +39,8 @@ echo "Getting terragrunt ${TERRAGRUNT_VERSION} ..."
 wget "https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64"
 wget "https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/SHA256SUMS"
 grep 'linux_amd64' < SHA256SUMS | sha256sum --check  --status
-mv "terragrunt_linux_amd64" terragrunt
-chmod +x terragrunt
-mv terragrunt ${BIN_DIR}
+mv "terragrunt_linux_amd64" terragrunt_binary
+chmod +x terragrunt_binary
+mv terragrunt_binary ${BIN_DIR}/terragrunt
 rm SHA256SUMS
 echo "Done downloading terragrunt ${TERRAGRUNT_VERSION}"
